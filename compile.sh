4e138b4aac80e05e99f74ad7d15ca43266acbdd3
fc777888 <you@example.com>
2022-03-16 10:21:11 +0800

    First commit.

---
+++ compile.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+# https://developer.android.google.cn/studio/releases/emulator?hl=zh-cn
+
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update && sudo apt-get install libx11-dev build-essential lld git ninja-build tree gpg gpg-agent flex bison python python3 libpython3-dev python3-pip curl wget locales crossbuild-essential-arm64 -y
+sudo apt-get update && sudo apt-get install build-essential lld git ninja-build tree gpg gpg-agent flex bison python python3 libpython3-dev python3-pip curl wget libgl1 libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev locales crossbuild-essential-arm64 -y
+
+sudo localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+sudo locale-gen en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+mkdir -p ~/.bin
+PATH="${HOME}/.bin:${PATH}"
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
+chmod a+rx ~/.bin/repo
+
+MYDIR=$(pwd)
+
+mkdir emu
+cd emu
+repo init -u https://android.googlesource.com/platform/manifest -b emu-master-dev --depth=1
+repo sync -qcj 12
+cd external/qemu
+pip install absl-py
+pip install urlfetch
+python android/build/python/cmake.py --noqtwebengine --noshowprefixforinfo --target linux_aarch64
+
+set +e
+
+echo Build emu done.
